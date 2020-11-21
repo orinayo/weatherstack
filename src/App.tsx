dd2ec4ac0dea386792538b720c8ea352fd829e22
Orinayo Oyelade <orinayooyelade@gmail.com>
2020-11-21 23:31:37 +0100

    add offline notification and normalize css. add common display, width and font classes to index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import {history} from 'utils/history'
+import {OfflineNotification} from './OfflineNotification'
 import { Counter } from './features/counter/Counter';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
